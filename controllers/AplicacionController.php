9908f114f35b29348b7143093663c3681eb5b06a
Roger <ronguerra51@gmail.com>
2024-08-19 08:45:25 -0600

    tabla aplicacion

---
+++ controllers/AplicacionController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace Controllers;
+
+use Exception;
+use Model\Aplicacion;
+use MVC\Router;
+
+class AplicacionController
+{
+    public static function index(Router $router)
+    {
+        $aplicaciones = Aplicacion::find(2);
+        $router->render('aplicaciones/index', [
+            'aplicaciones' => $aplicaciones
+        ]);
+    }
+
+    public static function guardarAPI()
+    {
+        $_POST['nombre'] = htmlspecialchars($_POST['nombre']);
+        try {
+            $aplicacion = new Aplicacion($_POST);
+            $resultado = $aplicacion->crear();
+            http_response_code(200);
+            echo json_encode([
+                'codigo' => 1,
+                'mensaje' => 'App guardado exitosamente',
+            ]);
+        } catch (Exception $e) {
+            http_response_code(500);
+            echo json_encode([
+                'codigo' => 0,
+                'mensaje' => 'Error al guardar App',
+                'detalle' => $e->getMessage(),
+            ]);
+        }
+    }
+
+    public static function buscarAPI()
+    {
+        try {
+            // ORM - ELOQUENT
+            // $aplicacion = aplicacion::all();
+            $aplicaciones = Aplicacion::obtenerAplicacionconQuery();
+            http_response_code(200);
+            echo json_encode([
+                'codigo' => 1,
+                'mensaje' => 'Datos encontrados',
+                'detalle' => '',
+                'datos' => $aplicaciones
+            ]);
+        } catch (Exception $e) {
+            http_response_code(500);
+            echo json_encode([
+                'codigo' => 0,
+                'mensaje' => 'Error al buscar Apps',
+                'detalle' => $e->getMessage(),
+            ]);
+        }
+    }
+
+    public static function modificarAPI()
+    {
+        $_POST['nombre'] = htmlspecialchars($_POST['nombre']);
+        $id = filter_var($_POST['app_id'], FILTER_SANITIZE_NUMBER_INT);
+        try {
+
+            $aplicacion = Aplicacion::find($id);
+            $aplicacion->sincronizar($_POST);
+            $aplicacion->actualizar();
+            http_response_code(200);
+            echo json_encode([
+                'codigo' => 1,
+                'mensaje' => 'App modificada exitosamente',
+            ]);
+        } catch (Exception $e) {
+            http_response_code(500);
+            echo json_encode([
+                'codigo' => 0,
+                'mensaje' => 'Error al modificar App',
+                'detalle' => $e->getMessage(),
+            ]);
+        }
+    }
+
+    public static function eliminarAPI()
+    {
+
+        $id = filter_var($_POST['id'], FILTER_SANITIZE_NUMBER_INT);
+        try {
+
+            $aplicacion = Aplicacion::find($id);
+            // $producto->sincronizar([
+            //     'situacion' => 0
+            // ]);
+            // $producto->actualizar();
+            $aplicacion->eliminar();
+            http_response_code(200);
+            echo json_encode([
+                'codigo' => 1,
+                'mensaje' => 'Producto eliminado exitosamente',
+            ]);
+        } catch (Exception $e) {
+            http_response_code(500);
+            echo json_encode([
+                'codigo' => 0,
+                'mensaje' => 'Error al eliminado producto',
+                'detalle' => $e->getMessage(),
+            ]);
+        }
+    }
+}
